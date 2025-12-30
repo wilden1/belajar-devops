0ff99579d2725de0eb08b3637a176de04e488349
Wildan Wafi <wildan.wafi@gmail.com>
2025-12-30 15:17:20 +0800

    Day 7: Change to Flask web server

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print("Hello dari dalam Container Docker!")
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<h1>App Wildan dah LIVE! 🚀</h1><p>Deploy guna CI/CD GitHub Actions & Docker.</p>"
+
+if __name__ == "__main__":
+    # Ini supaya app kau boleh ikut port yang cloud bagi nanti
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
